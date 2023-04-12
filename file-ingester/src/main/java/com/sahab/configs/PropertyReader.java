b71ae454b5aec06da28b5a9bba6e3da35e114c77
kiyaesi <kiyaesi@gmail.com>
2023-04-13 02:09:33 +0330

    add propertyReader for config properties

---
+++ file-ingester/src/main/java/com/sahab/configs/PropertyReader.java
@@ -0,0 +1,29 @@
+package com.sahab.configs;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Properties;
+
+public class PropertyReader {
+
+   public String readProperty(String fileUrl,String key){
+       String value;
+       try {
+            java.net.URL configFileUrl = ClassLoader.getSystemResource(fileUrl);
+
+            Properties props = new Properties();
+            props.load(configFileUrl.openStream());
+            value= props.getProperty(key);
+            return value;
+
+       } catch (FileNotFoundException ex) {
+           // file does not exist
+
+       } catch (Exception ex) {
+           // I/O error
+       }
+   return null;
+   }
+}
