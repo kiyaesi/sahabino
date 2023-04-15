988b3ac5480ff1cf80d33680093621019a130897
kiyaesi <kiyaesi@gmail.com>
2023-04-16 00:27:47 +0330

    edit log repositor to add query for finding log by time

--- rules-evaluator/src/main/java/com/sahab/Repository/LogWarningsRepositroy.java
+++ rules-evaluator/src/main/java/com/sahab/Repository/LogWarningsRepositroy.java
@@ -1,11 +1,11 @@
 package com.sahab.Repository;
 
 import com.sahab.Entity.LogWarningsEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.awt.print.Pageable;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface LogWarningsRepositroy extends JpaRepository<LogWarningsEntity, Long> {
@@ -14,5 +14,9 @@ public interface LogWarningsRepositroy extends JpaRepository<LogWarningsEntity, 
     List<LogWarningsEntity> findLogWarningsByWarningTypeSortByTime(String warningType,Pageable Pageable);
 
     @Query(value = "SELECT u BY LogWarningsEntity u WHERE u.system = ?1 ORDER BY logCreatedAt ")
-    List<LogWarningsEntity> findLogWarningsBySystemSortByTime(String warningType,Pageable Pageable);
+    List<LogWarningsEntity> findLogWarningsBySystemSortByTime(String System, Pageable Pageable);
+
+    @Query(value = "SELECT u BY LogWarningsEntity u WHERE u.system = ?1 and u.logCreatedAt between ?2  and ?3 ")
+    List<LogWarningsEntity> findLogWarningsBySystemByTime(String System, LocalDateTime logCreatedAt, LocalDateTime StartTime);
+
 }
